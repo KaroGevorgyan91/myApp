5cefb9ab26b10dc7f405934a8772088970bbc39a
Karo Gevorgyan <53303311+KaroGevorgyan91@users.noreply.github.com>
2019-12-04 22:01:18 +0400

    Updates

---
+++ precache-manifest.fc4f945d50a50e414efe78d38eca4b5a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fff903a771392a51c48dcc2948e75d7b",
+    "url": "/myApp/index.html"
+  },
+  {
+    "revision": "5eb5e217fa914ab5d2a5",
+    "url": "/myApp/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "7a2d4281ae7f2fc00496",
+    "url": "/myApp/static/js/2.a33aead3.chunk.js"
+  },
+  {
+    "revision": "5eb5e217fa914ab5d2a5",
+    "url": "/myApp/static/js/main.9257dfb1.chunk.js"
+  },
+  {
+    "revision": "20b3a6f0ff8f017f003d",
+    "url": "/myApp/static/js/runtime-main.32e33247.js"
+  }
+]);
